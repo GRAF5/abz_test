9b29d49a9a31a73b6e6e6995b48eeee3779e0d4c
GRAF5 <23andrei12@gmail.com>
2023-11-21 16:39:30 +0200

    add api requestts

--- app/server.js
+++ app/server.js
@@ -1,14 +1,56 @@
-const config = require('./config.json');
+"use strict";
+
+const express = require('express');
+const conf = require('./config.json');
 const DB = require('../models/index');
+const AuthenticationController = require('./controllers/authentication.controller');
+const ContentController = require('./controllers/content.controller');
+const { handler } = require('./classes/errors');
 
 class Server {
 
   constructor() {
-    this.config = process.env.CONFIG || config;
+    this.config = process.env.CONFIG || conf;
+    this.controllers = {};
   }
   
-  start () {
-    
+  start() {
+    this.server = express();
+    this.initControllers();
+    this.initRoutes();
+    this.server.listen(3000, () => {
+      console.log('Server listening on port 3000');
+    });
+    this.registerShutdown();
+  }
+
+  async stop() {
+
+  }
+
+  initControllers() {
+    const authentication = new AuthenticationController(this.config);
+    const content = new ContentController(this.config, authentication, DB.Position, DB.User);
+
+    this.controllers.authentication = authentication;
+    this.controllers.content = content;
+  }
+
+  initRoutes() {
+    this.server.use(express.json());
+
+    for (let controller of Object.values(this.controllers)) {
+      this.server.use('/', controller.router());
+    }
+    this.server.use(handler);
+  }
+
+  registerShutdown() {
+    let shutdown = () => this.stop()
+      .then(() => process.exit(), () => process.exit());
+      
+    process.on('SIGTERM', shutdown);
+    process.on('SIGINT', shutdown);
   }
 }
 
